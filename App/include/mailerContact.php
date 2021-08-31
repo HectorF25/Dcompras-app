f769bef08dff6d6a4087047d9f2aa445a9621cf7
Hector Florez <hectorflorez25@gmail.com>
2021-08-31 09:04:41 -0500

    release/HectorF25-upload-stable-project

--- App/include/mailerContact.php
+++ App/include/mailerContact.php
@@ -69,7 +69,7 @@ width: 100% !important;
 <tr>
 <td class="header" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; position: relative; padding: 25px 0; text-align: center;">
 <a href="http://localhost" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; position: relative; color: #3d4852; font-size: 19px; font-weight: bold; text-decoration: none; display: inline-block;">
-<img src="https://ibb.co/x8Wyy5k" class="logo" alt="Dcompras Logo" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; position: relative; max-width: 100%; border: none; height: 75px; max-height: 75px; width: 75px;">
+<img src="../assets/img/Recurso 4LogoDocumentacion.png" class="logo" alt="Dcompras" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; position: relative; max-width: 100%; border: none; height: 75px; max-height: 75px; width: 75px;">
 </a>
 </td>
 </tr>
