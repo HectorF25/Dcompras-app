ed31672a2dd59fb530297bd7b40ee6b6343a1cea
Santiago Perez <80767619+sp1014@users.noreply.github.com>
2021-09-25 23:39:51 -0500

    validations_user_forms

--- Public/Views/perfiladmin/administrador/editarPerfil.php
+++ Public/Views/perfiladmin/administrador/editarPerfil.php
@@ -79,75 +79,116 @@ $codigo = 6;
                         <li class="list-group-item p-3">
                             <div class="row">
                                 <div class="col">
-                                    <form action="?c=administrador&a=GuardarPerfil" method="POST" class="pro-form" id="pro-form" value="<?php echo $administradores->idUsuario; ?>" enctype="multipart/form-data" />
+                                <form action="?c=administrador&a=GuardarPerfil" method="POST" class="pro-form" id="pro-form" value="<?php echo $administradores->idUsuario; ?>" enctype="multipart/form-data" />
                                         <input type="hidden" id="idUsuario" name="idUsuario" value="<?php echo $administradores->idUsuario; ?>" />
                                         <input type="hidden" id="imgUsuario" name="imgUsuario" value="<?php echo $administradores->imgUsuario; ?>" />
-
-                                        <div class="form-row">
-                                            <div class="form-group col-md-6">
-                                                <label for="feFirstName">Nombre</label>
-                                                <input type="text" class="form-control" name="nombreUsuario" id="nombreUsuario" placeholder="Juan Camilo" value="<?php echo $administradores->nombreUsuario; ?>" required>
+  
+  
+ 
+                <!-- Grupo: Usuario -->
+                <div class="form-row">
+                <div class="form-group col-md-6">
+                                            <div class="formulario__grupo" id="grupo__nombreUsuario">
+                                                    <label for="nombreUsuario">Nombre</label>
+                                                    <div class="formulario__grupo-input">
+                                                        <input type="text" class="form-control nombreUsuario" name="nombreUsuario" id="nombreUsuario" placeholder="Juan Camilo" value="<?php echo $administradores->nombreUsuario; ?>" required>
+                                                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                                                    </div>
+                                                    <p class="formulario__input-error">El nombre debe tener un máximo de 40 caracteres y solo debe contener letras.</p>
+                                                </div>
                                             </div>
                                             <div class="form-group col-md-6">
-                                                <label for="feLastName">Apellido</label>
-                                                <input type="text" class="form-control" name="apellidoUsuario" id="apellidoUsuario" placeholder="Garzón Medina" value="<?php echo $administradores->apellidoUsuario; ?>" required>
+                                                <div class="formulario__grupo" id="grupo__apellidoUsuario">
+                                                    <label for="apellidoUsuario">Apellido</label>
+                                                    <div class="formulario__grupo-input">
+                                                        <input type="text" class="form-control apellidoUsuario" name="apellidoUsuario" id="apellidoUsuario" placeholder="Garzón Medina" value="<?php echo $administradores->apellidoUsuario; ?>" required>
+                                                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                                                    </div>
+                                                    <p class="formulario__input-error">El nombre debe tener un máximo de 40 caracteres y solo debe contener letras.</p>
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="form-row">
                                             <div class="form-group col-md-6">
-                                                <label for="feEmailAddress">Correo electrónico</label>
-                                                <input type="email" class="form-control" name="correoUsuario" id="correoUsuario" placeholder="juan@gmail.com" value="<?php echo $administradores->correoUsuario; ?>" required>
+                                                <div class="formulario__grupo" id="grupo__correoUsuario">
+                                                    <label for="correoUsuario">Correo electrónico</label>
+                                                    <div class="formulario__grupo-input">
+                                                        <input type="email" class="form-control correoUsuario" name="correoUsuario" id="correoUsuario" placeholder="juan@gmail.com" value="<?php echo $administradores->correoUsuario; ?>" required>
+                                                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                                                    </div>
+                                                    <p class="formulario__input-error">El email debe tener un máximo de 60 caracteres y puede contener letras, números y simbolos.</p>
+                                                </div>
                                             </div>
                                             <div class="form-group col-md-6">
-                                                <label for="fePassword">Contraseña</label>
-                                                <input type="password" class="form-control" name="contraseñaUsuario" id="contraseñaUsuario" placeholder="*******" value="<?php echo $administradores->contraseñaUsuario; ?>" required>
+                                                <div class="formulario__grupo" id="grupo__contraseñaUsuario">
+                                                    <label for="fePassword">Contraseña</label>
+                                                    <div class="formulario__grupo-input">
+                                                        <input type="password" class="form-control" name="contraseñaUsuario" id="contraseñaUsuario" placeholder="*******" value="<?php echo $administradores->contraseñaUsuario; ?>" required>
+                                                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                                                    </div>
+                                                    <p class="formulario__input-error">La contraseña debe tener un minímo de 4 caracteres y un máximo de 12 y puede contener letras, números y simbolos.</p>
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="form-row">
                                             <div class="form-group col-md-6">
-                                                <label for="feInputAddress">Fecha de nacimiento</label>
-                                                <input type="date" class="form-control" name="fechaNacimiento" id="fechaNacimiento" placeholder="07/15/2002" value="<?php echo $administradores->fechaNacimiento; ?>" required>
+                                                <div class="formulario__grupo" id="grupo__fechaNacimiento">
+                                                    <label for="feInputAddress">Fecha de nacimiento</label>
+                                                    <div class="formulario__grupo-input">
+                                                        <input type="date" class="form-control" name="fechaNacimiento" id="fechaNacimiento" placeholder="07/15/2002"  value="<?php echo $administradores->fechaNacimiento; ?>" required>
+                                                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                                                    </div>
+                                                </div>
                                             </div>
                                             <div class="form-group col-md-6">
-                                                <label for="feInputCity">Número de documento</label>
-                                                <input type="number" class="form-control" name="documentoUsuario" id="documentoUsuario" placeholder="1016757835" value="<?php echo $administradores->documentoUsuario; ?>" required>
+                                                <div class="formulario__grupo" id="grupo__documentoUsuario">
+                                                    <label for="feInputCity">Número de documento</label>
+                                                    <div class="formulario__grupo-input">
+                                                        <input type="number" class="form-control" name="documentoUsuario" id="documentoUsuario" placeholder="1016757835"  value="<?php echo $administradores->documentoUsuario; ?>" required>
+                                                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                                                        <p class="formulario__input-error">El numero de documento debe tener un minímo de 8 caracteres numericos y un máximo de 10.</p>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="form-row">
                                             <div class="form-group col-md-6">
                                                 <label for="feInputCity">Tipo de documento</label>
                                                 <select id="feInputState" class="form-control" id="idTipoDoc" name="idTipoDoc" placeholder=" " required>
-
-                                                    <option class="" value="<?php echo $administradores->idTipoDoc; ?>"><?php echo $administradores->nombreTipoDoc; ?></option>
+                                                <option class="formulario__label"value="<?php echo $administradores->idTipoDoc; ?>"><?php echo $administradores->nombreTipoDoc; ?></option>
                                                     <?php
                                                     foreach ($this->model->ListarTipoDoc() as $valores) : ?>
                                                         <option value="<?php echo $valores->idTipoDoc ?>"><?php echo $valores->nombreTipoDoc ?></option>
                                                     <?php endforeach; ?>
 
                                                 </select>
-
-                                                </select>
                                             </div>
                                             <div class="form-group col-md-6">
-                                                <label for="feInputAddress">Dirección</label>
-                                                <input type="text" class="form-control" name="direccionUsuario" id="direccionUsuario" placeholder="calle 28c sur 15-56 este" value="<?php echo $administradores->direccionUsuario; ?>" required>
+                                                <div class="formulario__grupo" id="grupo__direccionUsuario">
+                                                    <label for="feInputCity">Dirección</label>
+                                                    <div class="formulario__grupo-input">
+                                                        <input type="text" class="form-control" name="direccionUsuario" id="direccionUsuario" placeholder="calle 28c sur 15-56 este" value="<?php echo $administradores->direccionUsuario; ?>" required>
+                                                        <i class="formulario__validacion-estado fas fa-times-circle"></i>
+                                                        <p class="formulario__input-error">La direccion no es Correcta.</p>
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="form-row">
+                                        </div>   
+                                        
                                             <div class="form-group col-md-6">
+                                              
                                                 <label>Subir foto de perfil</label>
                                                 <input type="file" name="imgUsuario" id="imgUsuario" class="file-upload-default">
-                                                <div class="input-group ">
-                                                    <input type="text" class="form-control file-upload-info"  disabled value="<?php echo $administradores->imgUsuario; ?>">
+                    <div class="input-group ">
+                                                    <input type="text" class="form-control file-upload-info" disabled value="<?php echo $administradores->imgUsuario; ?>" >
                                                     <span class="input-group-append">
                                                         <button class="file-upload-browse btn btn-gradient-primary" type="button">Subir</button>
                                                     </span>
-                                                </div>
-                                                <div class="form-group col-md-6">
-                                                </div>
-                                            </div>
-                                        </div>
+                                                </div>                            
+                </div>
                                         <button type="submit" class="btn btn-gradient-primary mr-2" style="float: right;">Actualizar Datos</button>
+                                        
+                                        </div>
                                     </form>
                                 </div>
                             </div>
